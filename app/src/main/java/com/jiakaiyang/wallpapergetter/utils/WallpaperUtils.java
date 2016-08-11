1d493a7ab4adbc5424394c24a177d4286652d8d7
kaiyangjia <kaiyangjia@163.com>
2016-08-11 16:38:33 +0800

    add

--- app/src/main/java/com/jiakaiyang/wallpapergetter/utils/WallpaperUtils.java
+++ app/src/main/java/com/jiakaiyang/wallpapergetter/utils/WallpaperUtils.java
@@ -2,14 +2,45 @@ package com.jiakaiyang.wallpapergetter.utils;
 
 import android.app.WallpaperManager;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.content.res.Resources;
+import android.util.Log;
+import android.util.Pair;
 
 /**
  * Created by admin on 2016/8/8.
  */
 
 public class WallpaperUtils {
+    private static final String TAG = "WallpaperUtils";
 
     public static void getSystemWallpaper(Context context){
         WallpaperManager wallpaperManager = WallpaperManager.getInstance(context);
+    }
+
+
+    /*
+     * Finds a system apk which had a broadcast receiver listening to a particular action.
+     * @param action intent action used to find the apk
+     * @return a pair of apk package name and the resources.
+     */
+    public static Pair<String, Resources> findSystemApk(String action, PackageManager pm) {
+        final Intent intent = new Intent(action);
+        for (ResolveInfo info : pm.queryBroadcastReceivers(intent, 0)) {
+            if (info.activityInfo != null &&
+                    (info.activityInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0) {
+                final String packageName = info.activityInfo.packageName;
+                try {
+                    final Resources res = pm.getResourcesForApplication(packageName);
+                    return Pair.create(packageName, res);
+                } catch (PackageManager.NameNotFoundException e) {
+                    Log.w(TAG, "Failed to find resources for " + packageName);
+                }
+            }
+        }
+        return null;
     }
 }
--- app/src/main/java/com/jiakaiyang/wallpapergetter/wallpapers/WallpapersActivity.java
+++ app/src/main/java/com/jiakaiyang/wallpapergetter/wallpapers/WallpapersActivity.java
@@ -1,9 +1,13 @@
 package com.jiakaiyang.wallpapergetter.wallpapers;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Pair;
+import android.widget.TextView;
 
 import com.jiakaiyang.wallpapergetter.R;
+import com.jiakaiyang.wallpapergetter.utils.WallpaperUtils;
 
 /**
  * Created by admin on 2016/8/8.
@@ -15,5 +19,13 @@ public class WallpapersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        test();
+    }
+
+    private void test(){
+        Pair<String, Resources> result = WallpaperUtils.findSystemApk("com.android.launcher3.action.PARTNER_CUSTOMIZATION", getPackageManager());
+        String name = result.first;
+        TextView tv = (TextView) findViewById(R.id.txt_msg);
+        tv.setText(name);
     }
 }
